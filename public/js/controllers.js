dd2a144c8d0cacd4ffc6f5a00c213b2da2208ae8
Maurya Patel <info@itechinterface>
2022-12-23 19:28:18 +0530

    no message

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,27 +1,30 @@
 angular.module('starter.controllers', [])
 
     .factory('socket', function ($rootScope) {
-        var socket = io.connect('http://localhost:8081');
-        return {
-            on: function (eventName, callback) {
-                socket.on(eventName, function () {
-                    var args = arguments;
-                    $rootScope.$apply(function () {
-                        callback.apply(socket, args);
-                    });
-                });
-            },
-            emit: function (eventName, data, callback) {
-                socket.emit(eventName, data, function () {
-                    var args = arguments;
-                    $rootScope.$apply(function () {
-                        if (callback) {
-                            callback.apply(socket, args);
-                        }
-                    });
-                })
-            }
+        return{
+
         };
+        // var socket = io.connect('http://localhost:8081');
+        // return {
+        //     on: function (eventName, callback) {
+        //         socket.on(eventName, function () {
+        //             var args = arguments;
+        //             $rootScope.$apply(function () {
+        //                 callback.apply(socket, args);
+        //             });
+        //         });
+        //     },
+        //     emit: function (eventName, data, callback) {
+        //         socket.emit(eventName, data, function () {
+        //             var args = arguments;
+        //             $rootScope.$apply(function () {
+        //                 if (callback) {
+        //                     callback.apply(socket, args);
+        //                 }
+        //             });
+        //         })
+        //     }
+        // };
     })
 
     .directive('exportToCsv',function(){
